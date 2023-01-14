197cfabcd62780538f08be09179c5c7a244faa0c
= <=>
2023-01-14 22:21:27 +0530

    requirements.txt

--- init_setup.sh
+++ init_setup.sh
@@ -1,3 +1,9 @@
-import setuptools
-
-with open("README.md", "r", encoding="utf-8") as f:
+echo [$(date)]: "START"
+echo [$(date)]: "Creating conda env with python 3.8"
+pip install virtualenv
+python -m virtualenv venv
+echo [$(date)]: "activate env"
+venv/bin/activate
+echo [$(date)]: "intalling dev requirements"
+pip install -r requirements_dev.txt
+echo [$(date)]: "END"
