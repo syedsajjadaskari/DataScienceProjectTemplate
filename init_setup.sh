c3f2202f70ace4f4fb5993529d203d133163aada
syedsajjadaskari <syedsajjad62@gmail.com>
2023-01-15 07:17:37 +0530

    creating python envirnment

--- init_setup.sh
+++ init_setup.sh
@@ -1,9 +1,8 @@
 echo [$(date)]: "START"
 echo [$(date)]: "Creating conda env with python 3.8"
-pip install virtualenv
-python -m virtualenv venv
+conda create --prefix ./env python=3.8 -y
 echo [$(date)]: "activate env"
-venv/bin/activate
+source activate ./env
 echo [$(date)]: "intalling dev requirements"
 pip install -r requirements_dev.txt
 echo [$(date)]: "END"
